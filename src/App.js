74128f124a9e97a89f3afd2bf89a5908881b694e
Juan Navas <navascruzf@gmail.com>
2019-09-09 10:11:00 -0400

    Mod 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		let reading = {
+			name: 'Apples',
+			value: 23,
+			color: 'rgb(31, 90, 246)'
+		};
+
+		const parent = {
+			reading
+		};
+
+		return (
+			<div className="progress-bar">
+				<div className="value">
+					<div style={{ color: parent.reading.color, width: '5%' }}>
+						<span>{parent.reading.value}%</span>
+					</div>
+				</div>
+				{/* <div className="scale">
+					<div
+						className="graduation"
+						style={{ color: parent.reading.color, width: parent.reading.value + '%' }}
+					>
+						<span>|</span>
+					</div>
+				</div> */}
+				<div className="bar">
+					<div style={{ backgroundColor: parent.reading.color, width: parent.reading.value + '%' }} />
+					<div style={{ backgroundColor: '#d3d3d3', width: 100 - parent.reading.value + '%' }} />
+				</div>
+				{/* <div className="legend">
+					<div>
+						<span className="dot" style={{ color: parent.reading.color }}>
+							●
+						</span>
+						<span className="label">{parent.reading.name}</span>
+					</div>
+				</div> */}
+			</div>
+		);
+	}
 }
 
 export default App;
